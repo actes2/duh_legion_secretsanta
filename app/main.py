30e50554013259eb0b216ef57b11fe8c98fc4a38
actes2 <101230792+actes2@users.noreply.github.com>
2023-12-17 12:56:54 +0000

    Successfully implemented a backend for independent 'hosting' of the website. No high-level processing other than routing

--- app/main.py
+++ app/main.py
@@ -4,6 +4,19 @@ import requests as req
 from flask import Flask, render_template, request, flash, session
 
 
+app = Flask(__name__, static_folder="static")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
 if __name__ == "__main__":
     print("Init!")
-    
+
+    app.run("0.0.0.0", port=80, debug=True)
---
+++ github_workspace_setup.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+pip install Flask
+pip install requests
+echo "" && echo ""
+echo "Lookin' good you're all set to configure port 80 and run \"python app/main.py\""
