3d3461e281fa7e26fc4fda23b6087b67272dccab
actes2 <101230792+actes2@users.noreply.github.com>
2023-12-20 01:19:24 +0000

    added virtual environment 🐍

--- github_workspace_setup.sh
+++ github_workspace_setup.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 echo "Make sure to run this as an sudo user"
-pip install Flask
-pip install requests
-pip install pymongo
+source venv/bin/activate # We're sourcing our current shell to the virtual environment that was setup by python -m venv venv
+# To emulate the same experience: pip install -r requirements.txt
+#pip install Flask
+#pip install requests
+#pip install pymongo
 echo "" && echo ""
 echo "Lookin' good you're all set to configure port 80 and run \"./run_flask_server.sh\""
